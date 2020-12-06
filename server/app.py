c44f46dbe494727b137cd4f6175a6371ae7272d5
Erendz Tarakci <etarakci@berkeley.edu>
2020-12-05 20:27:26 -0800

    added models and started building out backend to load them

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_dotenv import DotEnv
+from helpers import sample
 
 import os
+import torch
 
 print("Initializing Backend")
 app = Flask(__name__, static_folder='build')
@@ -41,3 +43,35 @@ def static_proxy(path):
         return app.send_static_file(path)
     except:
         return app.send_static_file('index.html')
+
+
+# NLG Model delpoyment
+
+# import NLG models
+hp_model = torch.load('../stor/hp_model.pt')
+got_model = torch.load('../stor/got_model.pt')
+lotr_model = torch.load('../stor/lotr_model.pt')
+marvel_model = torch.load('../stor/marvel_model.pt')
+# connection_model = torch.load('../stor/connection_model.pt')
+
+@app.route("/predict", methods=["GET","POST"])
+def predict():
+    # This is where we get the genre from the user
+    #handle request here
+    source = "GET from side bar"
+    prompt = None
+
+    if source == "Harry Potter":
+        prompt = sample(hp_model,5)
+    elif source == "Game of Thrones":
+        prompt = sample(got_model,5)
+    elif source == "Lord of the Rings":
+        prompt == sample(lotr_model,5)
+    elif source == "Marvel":
+        prompt == sample(marvel_model,5)
+    # elif source == "Connection":
+    #     prompt == sample(connection_model,5)
+    print(prompt)
+
+    return jsonify(prompt)
+
--- server/helpers.py
+++ server/helpers.py
@@ -1,5 +1,11 @@
 import random
 import string
+import pickle
+import re
+import numpy as np
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
 
 
 def random_id_string(stringLength=6):
@@ -12,3 +18,44 @@ def random_number_string(stringLength=6):
     """Generate a random string of letters and digits """
     available_characters = string.digits
     return ''.join(random.choice(available_characters) for i in range(stringLength))
+
+
+# function to generate text
+def sample(net, size, prime='it is'):
+    # push to GPU
+    net.cuda()
+    net.eval()
+    # batch size is 1
+    h = net.init_hidden(1)
+    toks = prime.split()
+    # predict next token
+    for t in prime.split():
+      token, h = predict(net, t, h)
+    toks.append(token)
+    # predict subsequent tokens
+    for i in range(size-1):
+        token, h = predict(net, toks[-1], h)
+        toks.append(token)
+    return ' '.join(toks)
+
+def predict(net, tkn, h=None):
+  # tensor inputs
+  x = np.array([[token2int[tkn]]])
+  inputs = torch.from_numpy(x)
+  # push to GPU
+  inputs = inputs.cuda()
+  # detach hidden state from history
+  h = tuple([each.data for each in h])
+  # get the output of the model
+  out, h = net(inputs, h)
+  # get the token probabilities
+  p = F.softmax(out, dim=1).data
+  p = p.cpu()
+  p = p.numpy()
+  p = p.reshape(p.shape[1],)
+  # get indices of top 3 values
+  top_n_idx = p.argsort()[-3:][::-1]
+  # randomly select one of the three indices
+  sampled_token_index = top_n_idx[random.sample([0,1,2],1)[0]]
+  # return the encoded value of the predicted char and the hidden state
+  return int2token[sampled_token_index], h
