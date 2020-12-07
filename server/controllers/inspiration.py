8ec59cc3346f1b1bdb66a6cd61269f73b13f94b9
Erendz Tarakci <etarakci@berkeley.edu>
2020-12-06 21:10:28 -0800

    working on loading models and app.py

--- server/controllers/inspiration.py
+++ server/controllers/inspiration.py
@@ -4,9 +4,10 @@ import torch
 import torch.nn.functional as F
 import random
 import re
+import json
 
 # function to generate text
-def sample(net, size, prime='the'):
+def sample(net, size,token2int,int2token,prime='the'):
     # push to GPU
     net.cuda()
     net.eval()
@@ -15,15 +16,15 @@ def sample(net, size, prime='the'):
     toks = prime.split()
     # predict next token
     for t in prime.split():
-      token, h = predict(net, t, h)
+      token, h = predict(net, t, token2int, int2token, h)
     toks.append(token)
     # predict subsequent tokens
     for i in range(size-1):
-        token, h = predict(net, toks[-1], h)
+        token, h = predict(net, toks[-1], token2int, int2token, h)
         toks.append(token)
     return ' '.join(toks)
 
-def predict(net, tkn, h=None):
+def predict(net, tkn, token2int, int2token, h=None):
   # tensor inputs
   x = np.array([[token2int[tkn]]])
   inputs = torch.from_numpy(x)
@@ -46,18 +47,43 @@ def predict(net, tkn, h=None):
   return int2token[sampled_token_index], h
 
 # import NLG models
-hp_model = torch.load('../stor/hp_model.pt')
-got_model = torch.load('../stor/got_model.pt')
-lotr_model = torch.load('../stor/lotr_model.pt')
-marvel_model = torch.load('../stor/marvel_model.pt')
-# connection_model = torch.load('../stor/ChickenSoup_model.pt')
+# hp_net = WordLSTM() #not valid bc this function is in colab
+# hp_net.load_state_dict(torch.load('../stor/hp_model.pt'))
+with open('../stor/HP_token2int.json') as f:
+  hp_token2int = json.load(f)
+with open('../stor/HP_int2token.json') as f:
+  hp_int2token = json.load(f)
+
+# got_model = torch.load('../stor/got_model.pt')
+with open('../stor/GOT_token2int.json') as f:
+  got_token2int = json.load(f)
+with open('../stor/GOT_int2token.json') as f:
+  got_int2token = json.load(f)
+
+# lotr_model = torch.load('../stor/lotr_model.pt')
+with open('../stor/LOTR_token2int.json') as f:
+  lotr_token2int = json.load(f)
+with open('../stor/LOTR_int2token.json') as f:
+  lotr_int2token = json.load(f)
+
+# marvel_model = torch.load('../stor/marvel_model.pt')
+with open('../stor/marvel_token2int.json') as f:
+  marvel_token2int = json.load(f)
+with open('../stor/marvel_int2token.json') as f:
+  marvel_int2token = json.load(f)
+
+# cs_model = torch.load('../stor/ChickenSoup_model.pt')
+with open('../stor/ChickenSoup_token2int.json') as f:
+  cs_token2int = json.load(f)
+with open('../stor/ChickenSoup_int2token.json') as f:
+  cs_int2token = json.load(f)
 
 #import words
 hp_words = np.loadtxt("HP_wordbank.txt", delimiter=",", unpack=False)
 got_words = np.loadtxt("gGOT_wordbank.txt", delimiter=",", unpack=False)
 lotr_words = np.loadtxt("LOTR_wordbank.txt", delimiter=",", unpack=False)
 marvel_words = np.loadtxt("Marvel_wordbank.txt", delimiter=",", unpack=False)
-connection = np.loadtxt("CS_wordbank.txt", delimiter=",", unpack=False)
+# connection = np.loadtxt("CS_wordbank.txt", delimiter=",", unpack=False)
 
 def createWordbank(source,no_writers):
     n = 5 #n is the amount of words per writer
@@ -75,25 +101,33 @@ def createWordbank(source,no_writers):
 
     wordbank = random.choices(words, k=no_writers*n)
 
-    print(wordbank)
+    # print(wordbank)
 
     return wordbank
 
 def createPrompt(source):
-    prompt = None
-    if source == "Harry Potter":
-        prompt = sample(hp_model,5)
-    elif source == "Game of Thrones":
-        prompt = sample(got_model,5)
-    elif source == "Lord of the Rings":
-        prompt == sample(lotr_model,5)
-    elif source == "Marvel":
-        prompt == sample(marvel_model,5)
+    # prompt = None
+    # if source == "Harry Potter":
+    #     prompt = sample(hp_model,5,hp_token2int,hp_int2token)
+    # elif source == "Game of Thrones":
+    #     prompt = sample(got_model,5,got_token2int,got_int2token)
+    # elif source == "Lord of the Rings":
+    #     prompt == sample(lotr_model,5,lotr_token2int,lotr_int2token)
+    # elif source == "Marvel":
+    #     prompt == sample(marvel_model,5,marvel_token2int,marvel_int2token)
     # elif source == "Connection":
-    #   prompt = sample(connection_model,5)
+    #   prompt = sample(connection_model,5,cs_token2int,cs_int2token)
 
-    print(prompt)
+    # print(prompt)
 
-    return prompt
+    # return prompt
+    return "Prompt models under development"
+
+final_wordbank = createWordbank("Harry Potter",2)
+final_prompt = createPrompt("Harry Potter")
+
+print(final_wordbank)
+print(final_prompt)
+
 
 
